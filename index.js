205907c42eb205722fd172f71602d6182ecf8dda
Kaitlyn Middlebrook <kmiddlebrook2000@gmail.com>
2019-03-19 19:57:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ function logShout(string) {
 }
 
 function logWhisper(string) {
-  console.log(string.toLowerCase())
+  console.log(string.toLowerCase() === string)
 }
 
 function sayHiToGrandma(string) {
   if 
-    (console.log(string.toUpperCase()))
+    (console.log(string.toUpperCase() === string))
       return "YES INDEED!"
   else if
-    (string = "I love you, Grandma.")
+    (string === "I love you, Grandma.")
       return "I love you, too."
   else
       return "I can't hear you!"
